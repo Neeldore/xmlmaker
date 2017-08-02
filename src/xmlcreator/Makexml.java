e2c2cfc3dd6c5baaa35936b902462673e0f3e24d
neeldore <neeljain14@gmail.com>
2017-08-02 17:49:45 +0530

    first commit

--- src/xmlcreator/Makexml.java
+++ src/xmlcreator/Makexml.java
@@ -10,7 +10,6 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 /**
@@ -20,6 +19,7 @@ import org.w3c.dom.Element;
  */
 class Makexml {
 	private static final Logger mylogger = Logger.getLogger(Makexml.class.getName());
+	private static final String FILE_NAME = "out.xml";
 	private Makexml(){}   
 	/** 
 	 * @param args
@@ -29,36 +29,56 @@ class Makexml {
 				DocumentBuilderFactory builder =  DocumentBuilderFactory.newInstance();
 				DocumentBuilder docBuilder = builder.newDocumentBuilder();
 				Document doc = docBuilder.newDocument();
-				Element root = doc.createElement("students");
+				Element root = doc.createElement("root");
 				doc.appendChild(root);
-				
-				Attr attr = doc.createAttribute("type");
-				attr.setNodeValue("1");
-				root.setAttributeNode(attr);
-
-				Element firstname = doc.createElement("Firstname");
-				firstname.appendChild(doc.createTextNode("Neelkant"));
-				root.appendChild(firstname);
-
-				Element lastname = doc.createElement("Lastname");
-				lastname.appendChild(doc.createTextNode("jain"));
-				root.appendChild(lastname);
-
-				Element yob = doc.createElement("year_of_birth");
-				yob.appendChild(doc.createTextNode("13/06/1995"));
-				root.appendChild(yob);
-				
-				
-				TransformerFactory tf = TransformerFactory.newInstance();
-				Transformer trans = tf.newTransformer();
-				DOMSource doms = new DOMSource(doc);
-				File file = new File("out.xml");
-				StreamResult result = new StreamResult(file);
-				trans.transform(doms, result);
-				mylogger.info("file created ");
+				createElement(doc , "orderdetails" ,  root,"Neelkant" );
+				createElement(doc , "Lastname", root , "example");
+				createElement(doc , "year_of_birth" , root , "example3" );
+				transformIt(doc);
 			}
 			catch(Exception e){
 				mylogger.log(Level.FINE, "exception"+e);
 			}
+			
 		}
+		 /**
+		  * 
+		  * @param doc contains the document
+		  * @param elementname contains the element name
+		  * @param elementtext contains the element text 
+		  * @param appendto contains the element onto which you have to append the child 
+		  */
+		 public static void createElement(Document doc , String elementname  , Element appendto, String elementtext   ){
+			 Element temp = doc.createElement(elementname);
+			 temp.appendChild(doc.createTextNode(elementtext));
+			 appendto.appendChild(temp);
+		 }
+		 /**
+		  * 
+		  * @param doc 
+		  * @param elementname
+		  * @param appendto
+		  */
+		 public static void createElement(Document doc , String elementname ,  Element appendto  ){
+			 Element temp = doc.createElement(elementname);
+			 appendto.appendChild(temp);
+		 }
+		 /**
+		  * 
+		  * @param doc
+		  */
+		 public static void transformIt(Document doc){
+			 try{
+			 TransformerFactory tf = TransformerFactory.newInstance();
+				Transformer trans = tf.newTransformer();
+				DOMSource doms = new DOMSource(doc);
+				File file = new File(FILE_NAME);
+				StreamResult result = new StreamResult(file);
+				trans.transform(doms, result);
+				mylogger.info("file created ");
+			 }
+			 catch(Exception e){
+				 mylogger.info("file not created "+e);
+			 }
+		 }
 }
